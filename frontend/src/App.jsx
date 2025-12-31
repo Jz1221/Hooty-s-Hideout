fb0d7ff2e8d1e7e9554a3b30964e77356c99cf97
Jz1221 <jzheng0512@gmail.com>
2025-12-31 22:26:45 +0800

    feat: enhance product pages with loading spinner, product card, and create product functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { SignedIn, SignedOut, SignInButton, SignOutButton } from "@clerk/clerk-react"
 import Navbar from "./components/Navbar"
-import { Route, Routes } from "react-router"
+import { Route, Routes, Navigate } from "react-router"
 import HomePage from "./pages/HomePage"
 import ProductPage from "./pages/ProductPage"
 import EditProductPage from "./pages/EditProductPage"
@@ -10,7 +10,7 @@ import useAuthReq from "./hooks/useAuthReq"
 import useUserSync from "./hooks/useUserSync"
 
 function App() {
-  const { isClerkLoaded } = useAuthReq()
+  const { isClerkLoaded, isSignedIn } = useAuthReq()
   useUserSync()
   
   if (!isClerkLoaded) return null;
@@ -20,13 +20,15 @@ function App() {
         <Navbar />
         <main className="max-w-5xl mx-auto px-4 py-8">
           <Routes>
-            <Route path="/" element={<HomePage />}/>
-            <Route path="/product/:id" element={<ProductPage />}/>
-            <Route path="/profile" element={<ProfilePage />}/>
-            <Route path="/create" element={<CreatePage />}/>
-            <Route path="/edit/:id" element={<EditProductPage />}/>
-
-          </Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/product/:id" element={<ProductPage />} />
+          <Route path="/profile" element={isSignedIn ? <ProfilePage /> : <Navigate to={"/"} />} />
+          <Route path="/create" element={isSignedIn ? <CreatePage /> : <Navigate to={"/"} />} />
+          <Route
+            path="/edit/:id"
+            element={isSignedIn ? <EditProductPage /> : <Navigate to={"/"} />}
+          />
+        </Routes>
         </main>
       </div>
   )
